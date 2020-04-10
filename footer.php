7d50588c496c09233c2440fa54ccfeaa4193de9e
techsanjeev392 <63458617+techsanjeev392@users.noreply.github.com>
2020-04-10 19:28:36 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+<div class="wrapper col5">
+  <div id="footer">
+
+    <div id="copyright">
+      <p class="fl_left">Copyright &copy; 2020 - All Rights Reserved | <a href="adminlogin.php">Admin Login Panel</a> | <a href="doctorlogin.php">Doctor Login Panel</a></p>
+      <p class="fl_right">Developed by <a href="https://www.facebook.com/profile.php?id=100005114564749">Sanjeev Kumar</a></p>
+      <br class="clear" />
+    </div>
+    <div class="clear"></div>
+  </div>
+</div>
+</body>
+</html>
